1a92d0e55bcc91d653afe07a6e9de23b13d3cf41
jc7k <jeffchen72@gmail.com>
2019-01-29 09:23:15 -0800

    Update server.py
    Updated export_file_url to point to dropbox.com. Didn't work with Google Drive.

--- app/server.py
+++ app/server.py
@@ -8,8 +8,8 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-#export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1WnCSr-5VomKcrZcCFFBKv3NZyZuS1Cu'
+# export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/n3dudikr22ej1fb/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 # classes = ['black', 'grizzly', 'teddys']
