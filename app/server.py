7b5e3e73729cdaa880fcac51d2557c7054f47cd9
jc7k <jeffchen72@gmail.com>
2019-01-29 08:55:14 -0800

    Update server.py
    Updated export_file_url and classes to my own dog classifier.
    
    Recognized breeds are Japanese Chin, Husky, Samoyed, and Maltese. Error rate is around 2%

--- app/server.py
+++ app/server.py
@@ -8,9 +8,11 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+#export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=ttps://drive.google.com/open?id=1WnCSr-5VomKcrZcCFFBKv3NZyZuS1Cu'
 export_file_name = 'export.pkl'
 
+# classes = ['black', 'grizzly', 'teddys']
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
 
