2ea03fc889153c27db228131f51eb6a81d2afdfe
jc7k <jeffchen72@gmail.com>
2019-01-31 23:34:53 -0800

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,7 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-export_file_url = 'https://www.dropbox.com/s/gofssesxrqflyuz/export.pkl?dl=1'
+export_file_url = 'https://www.dropbox.com/s/43urb1hghowjcin/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
 classes = ['japanese_chin', 'samoyed', 'husky', 'maltese']
